5affa8575172176472ef92e21fcd62b8e75da7d0
Grzegorz Judas <grzegorz.judas@gmail.com>
2022-02-09 12:37:01 +0100

    Basic API working

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,13 @@
-console.log('hello world');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello world!');
+});
+
+const instance = app.listen(8081, () => {
+    console.log('Server listening on port 8081');
+}).on('error', (error) => {
+    console.error(error);
+});
